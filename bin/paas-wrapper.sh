fb44fd63807771bd198cc68c45abaed49381e5db
Jayesh Patel <ejayesh@hotmail.com>
2021-07-14 15:41:39 +0100

    apt buildpack install

--- bin/paas-wrapper.sh
+++ bin/paas-wrapper.sh
@@ -57,4 +57,11 @@ export AIRFLOW__API__AUTH_BACKEND=auth.api_auth_backend
 require_variable AIRFLOW__CORE__FERNET_KEY
 require_variable AIRFLOW__WEBSERVER__SECRET_KEY
 
+# The symbolic links to pg_restore and psql are not correctly setup for some reason
+rm /home/vcap/deps/0/bin/pg_dump
+ln -s /home/vcap/deps/0/apt/usr/lib/postgresql/12/bin/pg_dump /home/vcap/deps/0/bin/pg_dump
+
+rm /home/vcap/deps/0/bin/psql
+ln -s /home/vcap/deps/0/apt/usr/lib/postgresql/12/bin/psql /home/vcap/deps/0/bin/psql
+
 exec $@
