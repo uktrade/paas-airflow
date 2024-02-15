68517fde7944dcb56453e5b2dc5253af36b83b51
Kevin Carrogan <kevin@carrogan.com>
2024-02-15 14:59:03 +0000

    Fix pools command

--- bin/airflow-scheduler.sh
+++ bin/airflow-scheduler.sh
@@ -2,7 +2,7 @@
 
 airflow db upgrade
 
-airflow pools set default_pool ${AIRFLOW_DEFAULT_POOL_SLOTS:-16} "Main tasks"
-airflow pools set sensors ${AIRFLOW_SENSORS_POOL_SLOTS:-16} "External task sensors"
+airflow pools set default_pool ${AIRFLOW_DEFAULT_POOL_SLOTS:-16} "main-tasks"
+airflow pools set sensors ${AIRFLOW_SENSORS_POOL_SLOTS:-16} "external-task-sensors"
 
 airflow scheduler
