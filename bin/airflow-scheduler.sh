a9ab10f09018d527886491a73a2ccf9412adecc2
Kevin Carrogan <kevin@carrogan.com>
2024-02-15 14:24:37 +0000

    Use correct airflow commands

--- bin/airflow-scheduler.sh
+++ bin/airflow-scheduler.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-airflow upgradedb
+airflow db upgrade
 
-airflow pool -s default_pool ${AIRFLOW_DEFAULT_POOL_SLOTS:-16} "Main tasks"
-airflow pool -s sensors ${AIRFLOW_SENSORS_POOL_SLOTS:-16} "External task sensors"
+airflow pools -s default_pool ${AIRFLOW_DEFAULT_POOL_SLOTS:-16} "Main tasks"
+airflow pools -s sensors ${AIRFLOW_SENSORS_POOL_SLOTS:-16} "External task sensors"
 
 airflow scheduler
